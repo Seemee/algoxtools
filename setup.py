0971d425a2cfdfcd5f0401cdaddc1fd7dd04767a
Symen Hovinga <itsfull@hotmail.com>
2020-05-06 19:50:06 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="algoxtools",
-    version="0.1.0",
+    version="0.1.1",
     author="Symen Hillebrand Hovinga",
     author_email="itsfull@hotmail.com",
     description="A Fast Algorithm X implementation in Python using Numpy and Numba",
