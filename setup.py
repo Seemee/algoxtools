608751d979245588d1c887a52ed12efcd77d412c
Symen Hovinga <itsfull@hotmail.com>
2020-05-06 19:05:38 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="algoxtools",
+    version="0.1.0",
+    author="Symen Hillebrand Hovinga",
+    author_email="itsfull@hotmail.com",
+    description="A Fast Algorithm X implementation in Python using Numpy and Numba",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Seemee/algoxtools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
